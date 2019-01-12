6b64fa66fa33e796f05994d36c65bef91ab777b0
Mark Krutik <mkrutik@myseneca.ca>
2019-01-12 11:08:33 -0500

    Add files via upload

---
+++ group-app/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponentComponent } from "./home-component/home-component.component";
+
+const routes: Routes = [
+{ path: 'home', component: HomeComponentComponent },
+{ path: '', component: HomeComponentComponent }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
---
+++ group-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppContentComponent } from './app-content/app-content.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppContentComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    HomeComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
