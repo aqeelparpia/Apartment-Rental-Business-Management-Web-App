3d37b1b000a380e956f2678f9081222de0c7d76f
Daniel <dcameresekere@myseneca.ca>
2019-01-17 17:55:58 -0500

    Changes Made

--- group-app/src/app/app-routing.module.ts
+++ group-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponentComponent } from "./home-component/home-component.component";
+import { ContactComponentComponent } from './contact-component/contact-component.component';
 
 const routes: Routes = [
 { path: 'home', component: HomeComponentComponent },
-{ path: '', component: HomeComponentComponent }
+{ path: '', component: HomeComponentComponent },
+{ path: 'contact', component: ContactComponentComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
