5587d9515e3c983ad326995d676916fc718011e3
Mark Krutik <markkrutik@gmail.com>
2019-01-29 18:39:50 -0500

    recentFiles

---
+++ group-app/src/app/update-work-order/update-work-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-work-order',
+  templateUrl: './update-work-order.component.html',
+  styleUrls: ['./update-work-order.component.css']
+})
+export class UpdateWorkOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
